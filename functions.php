0eefc1748b969c241d06f878c42a6030182348e0
derwentx <derwentx@gmail.com>
2015-02-05 12:10:58 +0800

    style
    Put style in it’s own sheet,
    made header widgets backgroundless + brown

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
-	//Allow discounts to be specified based on how many liters of solution
-
+// TODO: Allow discounts to be specified based on how many liters of solution
+	
+/**
+ * Registers home page template widget areas
+ */
+function tanvas_widgets_init() {
+	// register_sidebar( array(
+	// 	'name' 	=> 'Home Doorway Buttons'))
+}
 ?>
