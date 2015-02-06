df68e43f9b1666235bd30e245aa0e50d7bcf6a32
derwentx <derwentx@gmail.com>
2015-02-06 11:48:03 +0800

    bunch of things
    joined footer widgets
    got rid of #main padding
    got rid of padding at bottom of icons
    change font on catalog pages
    slider title font
    change <a> font on widgets
    Bring secondary menu below primary

--- functions.php
+++ functions.php
@@ -2,10 +2,24 @@
 // TODO: Allow discounts to be specified based on how many liters of solution
 	
 /**
- * Registers home page template widget areas
+ * TODO: Registers home page template widget areas
  */
 function tanvas_widgets_init() {
 	// register_sidebar( array(
 	// 	'name' 	=> 'Home Doorway Buttons'))
 }
+
+/**
+ * Adds social media and newsletter icons to nav menu
+ */
+function tanvas_add_social_icons () {
+	echo "<ul><li class='fr fa-facebook'><i class='facebook-official'></i></lu></ul>";
+}
+//add_action( 'woo_nav_inside', 'tanvas_add_social_icons', 20);
+
+function woo_options_add($options){
+	//if(WP_DEBUG) error_log("woo_options_add called with :".serialize($options));
+	return $options;
+}
+
 ?>
